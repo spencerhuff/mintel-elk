e8f17685ea0ad55d762dd6dbbf96440dbf050752
Spencer Huff <spencerLhuff@gmail.com>
2021-10-17 22:38:57 -0700

    Added support for BSD netcat

--- runGNU.sh
+++ runGNU.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Using GNU netcat (CentOS, Fedora, MacOS Homebrew, ...)
 docker-compose up -d
 sleep 120 # wait 2 minutes for the stack to initialize before sending log data
 curl -L -k https://github.com/mintel/sre-interview-assets/blob/master/challenges/challenge_01_docker_elk_apache_logs/WEB_access_log.log.gz?raw=true | gzip -d | nc -c localhost 5000
